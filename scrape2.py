b0c4df75802125cfe3cb158945a415321d5dedfe
Sofia Escoto <seescoto2000@gmail.com>
2022-07-29 18:16:21 -0400

    370 qs done

--- scrape2.py
+++ scrape2.py
@@ -34,13 +34,40 @@ questions = []
 answers = []  
 
 for i in range(len(qs)):
-    questions.append(qs[i].text.strip())
+    questions.append(qs[i].text.strip('.').strip())
     answers.append(ans[i].text.strip())
     
     answers[i] = answers[i].split('Answer: ')[-1] #get end of string after answer: 
     
-df3 = pd.dataFrame(list(zip(questions, answers)), columns = ['question', 'truthValue'])  
+df3 = pd.DataFrame(list(zip(questions, answers)), columns = ['question', 'truthValue'])  
 
 '''
+70 t/f questions from cosmopolitan.com
+
+/uk/worklife not disallowed on robots.txt, good to scrape
 
 '''      
+
+url = "https://www.cosmopolitan.com/uk/worklife/a32612392/best-true-false-quiz-questions/"
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser') 
+
+#questions and answers in separate sections, but both in ol class = "body-ol"
+blog = soup.find_all('ol', class_ = "body-ol") 
+
+questions = [] #format fine
+answers = [] #format = TRUTHVALUE - explanation
+
+#tried to use indeces so i could use one for loop but kept getting errors
+#so i used two just for the sake of time
+for q in blog[0]:
+    questions.append(q.text.strip('.').strip()) 
+
+for a in blog[1]:
+    answers.append(a.text.split('-')[0].strip())
+
+df4 = pd.DataFrame(list(zip(questions, answers)), columns = ['question', 'truthValue']) 
+
+main = pd.concat([df3, df4], ignore_index = True)
+
+
