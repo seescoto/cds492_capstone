82be2f1c686b7c6687163c8b0afdd9d12e24704c
seescoto <sescoto@gmu.edu>
2022-07-31 23:54:56 -0400

    all qs done

--- scrape3.py
+++ scrape3.py
@@ -18,7 +18,7 @@ from bs4 import BeautifulSoup
 robots.txt doesnt disallow anything, good to scrape
 '''
 
-"""
+
 url = "https://bigquizthing.com/trivia-questions-ans/general-knowledge-45-true-or-false-questions-and-answers/"
 page = requests.get(url) 
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -50,26 +50,6 @@ for i in range(len(all_ps)):
 
 df1 = pd.DataFrame(list(zip(qs, ans)), columns = ['question', 'truthValue'])
 
-"""
-'''
-150 t/f questions from momjunction.com
-
-/articles not disallowed on robots.txt, good to scrape
-'''
-
-"""
-url = "https://www.momjunction.com/articles/true-or-false-questions-for-kids-answers_00695346"
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser') 
-blog = soup.find_all('p')
-"""
-
-#request could not be satisfied, do later
-
-
-#https://www.momjunction.com/articles/true-or-false-questions-for-kids-answers_00695346/
-#get all p, remove any strings that start with 'right answer:' then sort by parity
-#questions numbered, split by '.', 1 [1] to get second part aka the question
 
 
 
@@ -84,20 +64,68 @@ soup = BeautifulSoup(requests.get(url).content, 'html.parser')
 blog = soup.find_all('p')
 #take out last three and first three
 blog = blog[3:-3]
-print(blog[0])
 
 qs = []
 ans = []
 for i in range(len(blog)):
     #even index is question , take out number
-    #format is #. QESTION
+    #format is #. QUESTION
     if i % 2 == 0:
         q = blog[i].text.split('.', 1)[1]
         qs.append(q.strip())
     #odd index is answer, no extra formatting necessary 
     else:
         ans.append(blog[i].text)
-    
+
 #print(len(qs) == len(ans))
 
+        
+df2 = pd.DataFrame(list(zip(qs, ans)), columns = ['question', 'truthValue'])
 
+
+'''
+60 t/f questions from ponly.com 
+
+only /wp-admin disallowed on robots.txt, good to scrape
+'''
+url = "https://ponly.com/true-or-false-questions/"
+soup = BeautifulSoup(requests.get(url).content, 'html.parser')
+blog = soup.find('div', class_ = "entry-content")
+ps = blog.find_all('p')
+
+#skip first 7 and last two
+ps = ps[7:-2]
+
+#questions format: #. QUESTION
+#answers format: A: True
+#or 
+#A: False. explanation
+#also some empty strings with just a space, .text makes it \xa0
+
+ps = [p for p in ps if p.text != "\xa0"]
+qs = [] 
+ans = []
+
+#some extra tags (5), sort by if contains A: or next p contants A:
+for i in range(len(ps)):
+    if (i + 1 < len(ps)) and (ps[i+1].text.startswith('A:')):
+        q = ps[i].text.split('.', 1)[1] #get after number
+        qs.append(q.strip())
+    elif ps[i].text.startswith('A:'):
+        a = ps[i].text.replace('A: ','')
+        a = a.split('.')[0]
+        ans.append(a.strip())
+    else:
+        continue
+    
+df3 = pd.DataFrame(list(zip(qs, ans)), columns = ['question', 'truthValue'])
+                
+main = pd.read_csv('qa.csv') 
+
+main = pd.concat([main, df1, df2, df3], ignore_index = True)
+
+main.to_csv('qa.csv')
+        
+        
+        
+        
